05ac1a98293dd697b4314ce996bbf20bdb2cc9b8
Sean Chen <skypemaster007@gmail.com>
2019-06-20 18:25:52 -0700

    Cleanup in tests file

--- tests/tests.rs
+++ tests/tests.rs
@@ -18,16 +18,8 @@ fn test_creation() {
 
 #[test]
 fn test_merge_to_completion() {
-    let mut lbuf: VecDeque<i32> = VecDeque::new();
-    let mut rbuf: VecDeque<i32> = VecDeque::new();
-
-    lbuf.push_back(1);
-    lbuf.push_back(3);
-    lbuf.push_back(5);
-
-    rbuf.push_back(2);
-    rbuf.push_back(4);
-    rbuf.push_back(6);
+    let lbuf: VecDeque<i32> = vec![1,3,5].into_iter().collect(); 
+    let rbuf: VecDeque<i32> = vec![2,4,6].into_iter().collect(); 
 
     let mut funnel = Funnel::new_with_buffers(lbuf, rbuf);
     funnel.merge_to_completion();
